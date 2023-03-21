bf74b94945903b98bd058de9e3c5b56d547f8399
Erhan <demirci.erhan@windowslive.com>
2023-03-21 21:23:41 +0300

    dependencies setting
    dependencies setting

--- Package.swift
+++ Package.swift
@@ -17,19 +17,13 @@ let package = Package(
         .package(
               url: "https://github.com/apple/swift-atomics.git",
               .upToNextMajor(from: "1.0.0"))// or `.upToNextMinor
-        ,
-        .package(
-                  url: "https://github.com/Netvent/storyly-ios.git",
-                  .upToNextMajor(from: "1.0.0"))// or `.upToNextMinor
-        
-       
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "segmentify",
-            dependencies: [.product(name: "Atomics", package: "swift-atomics"),.product(name: "Storyly", package: "storyly-ios")],
+            dependencies: [.product(name: "Atomics", package: "swift-atomics")],
             path: "Sources"),
         .testTarget(
             name: "segmentifyTests",
